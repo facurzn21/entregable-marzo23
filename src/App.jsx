e54504385fd0170d61997c8fa362111fc9291503
facurzn21 <facundoeze@gmail.com>
2023-08-16 21:42:18 -0300

    Agregando logica de consigna

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
+import React, { useState } from "react";
+import Card from "./Card";
+
 function App() {
-  //Aqui deberias agregar los estados y los handlers para los inputs
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name.trim().length < 3 || color.length !== 7 /* longitud HEX */) {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+      setSubmitted(true);
+    }
+  };
 
   return (
     <div className="App">
-      <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
+      <h1>Bienvenido a nuestro sitio de colores favoritos!</h1>
+      {!isValid && <p>Por favor chequea que la información sea correcta</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Ingresa tu nombre
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          Ingresa tu color favorito (en formato HEX)
+          <input
+            type="text"
+            onChange={(e) => setColor(e.target.value)}
+            value={color}
+          />
+        </label>
+        <button type="submit">Enviar</button>
+      </form>
+      {submitted && isValid && <Card name={name} color={color} />}
     </div>
   );
 }
+
+
 
 export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,22 @@
+import React from "react";
+
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+function Card({ name, color }) {
   return (
-    <div>
-      <h2>Esto es un componente</h2>
+    <div
+      style={{
+        padding: "20px",
+        backgroundColor: color,
+        color: '#fff',
+        width: '200px',
+        textAlign: 'center',
+        borderRadius: '10px'
+      }}
+    >
+      <h2>Hola, {name}!</h2>
+      <p>Sabemos que tu color favorito es: {color}</p>
     </div>
   );
 }
